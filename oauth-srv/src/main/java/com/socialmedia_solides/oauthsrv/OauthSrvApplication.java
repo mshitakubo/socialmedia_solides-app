61779a1d27ebc2510e8772884e667fb6d8a10214
mshitakubo <nostressmeister@gmail.com>
2023-11-11 23:57:47 -0300

    Project OAuth created

---
+++ oauth-srv/src/main/java/com/socialmedia_solides/oauthsrv/OauthSrvApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia_solides.oauthsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSrvApplication.class, args);
+	}
+
+}
---
+++ oauth-srv/src/test/java/com/socialmedia_solides/oauthsrv/OauthSrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmedia_solides.oauthsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
